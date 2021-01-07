0f5f147011005bf085f682adb1ef38036f00fae0
Alex Pritchard <alexander.g.pritchard@gmail.com>
2021-01-07 03:16:54 +0000

    Add creature movement and controls

--- src/creature/creature.gd
+++ src/creature/creature.gd
@@ -1,2 +1,23 @@
 extends KinematicBody2D
 
+export(float) var speed := 200.0
+
+var velocity := Vector2.ZERO
+
+func _physics_process(_delta: float) -> void:
+	# reset velocity
+	velocity = Vector2.ZERO
+	
+	# get input and set velocity direction
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1.0
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1.0
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1.0
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1.0
+	
+	# calculate velocity and move
+	velocity = velocity.normalized() * speed
+	velocity = move_and_slide(velocity)
