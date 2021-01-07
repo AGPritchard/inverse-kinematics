d2b756cb333aae68fc172d5aed4130cf44144d03
Alex Pritchard <alexander.g.pritchard@gmail.com>
2021-01-07 02:32:16 +0000

    Add segments creation

--- src/tentacle/tentacle.gd
+++ src/tentacle/tentacle.gd
@@ -1,2 +1,13 @@
 extends Node2D
 
+const NUM_OF_POINTS := 16
+const SEGMENT_LENGTH := 5
+
+var segments := []
+
+func _ready() -> void:
+	# create a list of segments (a list of points)
+	for i in NUM_OF_POINTS:
+		segments.append((Vector2(0, SEGMENT_LENGTH) * i) + 
+		(Vector2.UP * SEGMENT_LENGTH * (NUM_OF_POINTS - 1)))
+	
